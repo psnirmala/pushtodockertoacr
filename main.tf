ac0e91ced5a2bbaa8f7a8f38a0b8b41349537101
psnirmala <periasamy.subramanian@gmail.com>
2026-07-13 07:26:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,18 +12,69 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "python-app-rg"
+  name     = "rg-python-app"
   location = "East US"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "pythondockerregistry2026111" # Must be globally unique
+  name                = "acrpythonreg001" # Must be globally unique
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
-  admin_enabled       = false # Disabled for security; we will use the Service Principal instead
+  admin_enabled       = true
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-python-app"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "ca-env-python"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = "python-web-app"
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  registry {
+    server               = azurerm_container_registry.acr.login_server
+    username             = azurerm_container_registry.acr.admin_username
+    password_secret_name = "registry-password"
+  }
+
+  secret {
+    name  = "registry-password"
+    value = azurerm_container_registry.acr.admin_password
+  }
+
+  template {
+    container {
+      name   = "pythonapp"
+      image  = "${azurerm_container_registry.acr.login_server}/python-app:latest"
+      cpu    = "0.25"
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 8000
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+}
+
+output "container_app_url" {
+  value = azurerm_container_app.app.ingress[0].fqdn
 }
